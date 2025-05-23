9329ee3d3610df168f3789f943929182d76264ec
jetlin6409 <82524237+jetlin6409@users.noreply.github.com>
2025-05-23 17:06:18 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,43 +110,43 @@ const API_SITES = {
         name: 'iKun资源'
     },
     ckzy: {
-        api: 'https://www.ckzy1.com/api.php/provide/vod/?ac=list',
+        api: 'https://www.ckzy1.com/api.php/provide/vod/',
         name: 'CK资源',
         adult: true
     },
     jkun: {
-        api: 'https://jkunzyapi.com/api.php/provide/vod/?ac=list',
+        api: 'https://jkunzyapi.com/api.php/provide/vod/',
         name: 'jkun资源',
         adult: true
     },
     bwzy: {
-        api: 'https://api.bwzym3u8.com/api.php/provide/vod/?ac=list',
+        api: 'https://api.bwzym3u8.com/api.php/provide/vod/',
         name: '百万资源',
         adult: true
     },
     souav: {
-        api: 'https://api.souavzy.vip/api.php/provide/vod/?ac=list',
+        api: 'https://api.souavzy.vip/api.php/provide/vod/',
         name: 'souav资源',
         adult: true
     },
     r155: {
-        api: 'https://155api.com/api.php/provide/vod/?ac=list',
+        api: 'https://155api.com/api.php/provide/vod/',
         name: '155资源',
         adult: true
     },
     lsb: {
-        api: 'https://apilsbzy1.com/api.php/provide/vod/?ac=list',
+        api: 'https://apilsbzy1.com/api.php/provide/vod/',
         name: 'lsb资源',
         adult: true
     },
     huangcang: {
-        api: 'https://hsckzy.vip/api.php/provide/vod/?ac=list',
+        api: 'https://hsckzy.vip/api.php/provide/vod/',
         name: '黄色仓库',
         adult: true,
-        detail: 'https://hsckzy.vip/api.php/provide/vod/?ac=list'
+        detail: 'https://hsckzy.vip/api.php/provide/vod/'
     },
     yutu: {
-        api: 'https://yutuzy10.com/api.php/provide/vod/?ac=list',
+        api: 'https://yutuzy10.com/api.php/provide/vod/',
         name: '玉兔资源',
         adult: true
     },
